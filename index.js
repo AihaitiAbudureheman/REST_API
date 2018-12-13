55265d30677246c86fc399f5ca7503dc0d36918c
Aihaiti Abudureheman <info@kootkot.com>
2018-12-13 22:35:24 +0200

    Learned about URL and path

--- index.js
+++ index.js
@@ -5,3 +5,25 @@
 // Dependencies
 
 var http = require("http");
+var url = require("url");
+
+// The server should respond to all request with a string
+var server = http.createServer(function(req, res) {
+  // Get the URL and parse it
+  var parsedUrl = url.parse(req.url, true);
+
+  //Get the path
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, "");
+
+  // Send the reponse
+  res.end("Hello world");
+
+  // Log the request path
+  console.log("request received on path: " + trimmedPath);
+});
+
+// start the server
+server.listen(3000, function() {
+  console.log("Server is listening on port 3000");
+});
